0940a4528b2d247ea7cbc95c5eed6d2c4bda8729
JoSantana0005 <joseasantana05@gmail.com>
2024-12-10 23:10:51 -0400

    A small one was added to return to the dashboard, it was added to all the scripts, I will continue tomorrow with the header and sidebar designs ready

---
+++ public/Js/Pages/Friends.js
@@ -0,0 +1,10 @@
+//Evento para volver al dashboard
+let dashboard = document.getElementById('Dashboard');
+dashboard.addEventListener('click', () =>{
+    const enlace = '../../../index.html'
+    if(dashboard){
+        window.location.href = enlace
+    }else{
+        console.log("Hubo un error")
+    }
+})
