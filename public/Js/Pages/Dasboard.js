df49d41fa7f3ad125393d32bc0ed8ebb01e9159f
JoSantana0005 <joseasantana05@gmail.com>
2024-12-11 21:09:14 -0400

    Some events are added to the dashboard and store js, the store design is already finished, Games, friends and events are missing

--- public/Js/Pages/Dasboard.js
+++ public/Js/Pages/Dasboard.js
@@ -1,9 +1,20 @@
-//Evento para volver al dashboard
-let dashboard = document.getElementById('Dashboard');
-dashboard.addEventListener('click', () =>{
-    const enlace = '../../../index.html'
-    if(dashboard){
-        window.location.href = enlace
+//Evento Para mover el contenido de recommend of you
+let arrow_left = document.getElementById('Arrow-left');
+let arrow_right = document.getElementById('Arrow-Right');
+let VideoGame_recommend = document.getElementById('Recommend--Videogame');
+
+// Arrow-left
+arrow_left.addEventListener('click',()=>{
+    if(arrow_left){
+        VideoGame_recommend.scrollLeft -= 285;
+    }else{
+        console.log("Hubo un error")
+    }
+})
+// Arrow-Right
+arrow_right.addEventListener('click',()=>{
+    if(arrow_right){
+        VideoGame_recommend.scrollLeft += 285;
     }else{
         console.log("Hubo un error")
     }
--- public/Js/Pages/Store.js
+++ public/Js/Pages/Store.js
@@ -1,3 +1,26 @@
+let cont = 1
+let Remove = document.getElementById('Remove');
+let Add = document.getElementById('Add');
+//Function para disminuir la cantidad de juegos
+function RemoveCont(){
+    const Amount = document.getElementById('Amount');
+    Remove.addEventListener('click',()=>{
+        if(cont <= 0){
+            alert("Please")
+        }else{
+            cont--;
+            Amount.textContent = cont
+        }
+    })
+}
+//Function para aumentar la cantidad de juegos
+function AddCont(){
+    const Amount = document.getElementById('Amount');
+    Add.addEventListener('click',()=>{
+        cont++;
+        Amount.textContent = cont
+    })
+}
 //Evento para volver al dashboard
 let dashboard = document.getElementById('Dashboard');
 dashboard.addEventListener('click', () =>{
@@ -8,3 +31,22 @@ dashboard.addEventListener('click', () =>{
         console.log("Hubo un error")
     }
 })
+// Evento para abrir la ventana modal
+let VideoGames = document.querySelectorAll('.VideoGame');
+let modal = document.getElementById('Modal--Window');
+
+VideoGames.forEach(VideoGame =>{
+    VideoGame.addEventListener('click',()=>{
+        modal.showModal();
+    });
+})
+// Evento para cerrar la ventana Modal
+let close_Modal = document.getElementById('Close--modal');
+close_Modal.addEventListener('click',() =>{
+    const Amount = document.getElementById('Amount');
+    Amount.textContent = 1;
+    modal.close();
+})
+
+RemoveCont();
+AddCont();
