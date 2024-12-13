67ef568865d1ee5156a0adeae9c08f4310708831
JoSantana0005 <joseasantana05@gmail.com>
2024-12-13 12:06:19 -0400

    Putting the images of the games in the medainte api

--- public/Js/Pages/Dasboard.js
+++ public/Js/Pages/Dasboard.js
@@ -21,28 +21,73 @@ arrow_right.addEventListener('click',()=>{
         console.log("Hubo un error")
     }
 })
-// Conexion a la api rawg
-const Api = async () =>{
+let games_popular = '';
+let recommend_games = '';
+let Most_played = '';
+// Conexion a la api para obtener los juegos mas jugados
+const Obtener_jugados = async() =>{
     try{
-        const repuesta = await fetch(`https://api.rawg.io/api/games?key=${key}`);
-        console.log(repuesta);
-        let games_popular = '';
-        if(repuesta.status == 200){
-            const datos = await repuesta.json();
+        const respuesta = await fetch(`https://api.rawg.io/api/games?key=${key}&dates=2020-01-01,2024-01-01`);
+        console.log(respuesta)
+        if(respuesta.status == 200){
+            const datos = await respuesta.json();
+            const Mosts = datos.results
+            Mosts.forEach(Most =>{
+                Most_played += `<article>
+                    <img src="${Most.background_image}" alt="VideoGame">
+                    <div>
+                        <span id="Name--videoGame">${Most.name}</span>
+                        <span id="Most--played">${Most.added}</span>
+                    </div>
+                </article>`
+            })
+            document.getElementById('Most--videoGame').innerHTML = Most_played
+        }
+    }catch(e){
+        console.error(`Hubo un error ${e}`)
+    }
+}
+// Conexion a la api para obtener los juegos de GOTY
+const Obtener_Goty = async() =>{
+    try{
+        const respuesta = await fetch(`https://api.rawg.io/api/games?key=${key}&dates=2012-01-12,2023-01-12`);
+        if(respuesta.status == 200){
+            const datos = await respuesta.json();
+            const recommend = datos.results;
+            recommend.forEach(game =>{
+                recommend_games += `<div>
+                        <img src="${game.background_image}" alt="Imagen" id="Imagen--recommed">
+                        <h3 id="Title--recommed">${game.name}</h3>
+                    </div>`
+            })
+            document.getElementById('videogame--recommed').innerHTML = recommend_games;
+        }
+    }catch(e){
+        console.error(`Hubo error ${e}`);
+    }
+}
+// Conexion a la api para obtner los juegos mas jugados hasta el momento
+const Obtener_juegos = async() =>{
+    try{
+        const respuesta = await fetch(`https://api.rawg.io/api/games?key=${key}&dates=2024-01-01,2024-12-01&ordening=-added`);
+        if(respuesta.status == 200){
+            const datos = await respuesta.json();
             console.log(datos)
             const games = datos.results
-            games.forEach(element => {
-                games_popular += `<div>
-                        <img src="${element.background_image}" alt="Logo">
-                        <h4>${element.name}</h4>
+            //foreach
+            games.forEach(game =>{
+            games_popular += `<div>
+                        <img src="${game.background_image}" alt="Logo">
+                        <h4>${game.name}</h4>
                     </div>`
             });
             document.getElementById('Games--pop').innerHTML = games_popular;
         }
-    
-    
-    }catch (e){
-        console.error(`Hubo un error en la conexion ${e}`)
+    }catch(e){
+
     }
 }
-Api();
+
+Obtener_jugados();
+Obtener_juegos();
+Obtener_Goty();
