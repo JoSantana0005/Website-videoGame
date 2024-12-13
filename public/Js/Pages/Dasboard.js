e2ca7d478dea384a008af891df70d115299107f3
JoSantana0005 <joseasantana05@gmail.com>
2024-12-13 11:19:24 -0400

    first connection ready

--- public/Js/Pages/Dasboard.js
+++ public/Js/Pages/Dasboard.js
@@ -1,3 +1,5 @@
+const key = 'd64b911edb634a9ca476c56451fc1fb4';
+const id = '20';
 //Evento Para mover el contenido de recommend of you
 let arrow_left = document.getElementById('Arrow-left');
 let arrow_right = document.getElementById('Arrow-Right');
@@ -20,11 +22,25 @@ arrow_right.addEventListener('click',()=>{
     }
 })
 // Conexion a la api rawg
-
 const Api = async () =>{
     try{
-        const repuesta = await fetch('https://api.rawg.io/api/platforms?key=d64b911edb634a9ca476c56451fc1fb4');
+        const repuesta = await fetch(`https://api.rawg.io/api/games?key=${key}`);
         console.log(repuesta);
+        let games_popular = '';
+        if(repuesta.status == 200){
+            const datos = await repuesta.json();
+            console.log(datos)
+            const games = datos.results
+            games.forEach(element => {
+                games_popular += `<div>
+                        <img src="${element.background_image}" alt="Logo">
+                        <h4>${element.name}</h4>
+                    </div>`
+            });
+            document.getElementById('Games--pop').innerHTML = games_popular;
+        }
+    
+    
     }catch (e){
         console.error(`Hubo un error en la conexion ${e}`)
     }
